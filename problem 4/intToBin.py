643090f9aacad8c7b76f3a5f2d0f793276828eb7
Referee4digit <chyj3442@gmail.com>
2024-11-01 15:54:42 -0500

    Q4 ff, Q5 mid

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -1,25 +1,28 @@
 def int_to_reverse_binary(num1):
     binary_val = ''
 #write your while loop here
-    while num1 > 0:
-        print (num1%2)
-        num1 = num1//2
-
-    return binary_val;
+    while num1>0:
+        x=num1 % 2
+        binary_val=binary_val+str(x)
+    return binary_val
 
 
 def string_reverse(input_string): 
     reverse_input = ''
-    
-   #write your for loop here
-    for 
-    
+    length=len(int_to_reverse_binary(num1))
+    john=length-1
+    empty=[0]*length
+    for i in int_to_reverse_binary(num1):
+        empty[john]=i
+        john-=1
+    reverse_input=''.join(empty)
     return reverse_input
 
 if __name__ == '__main__':
-    user_input = int(input())
+    print("Enter the number: ")
+    num1 = int(input())
     
-    binary_string = str(int_to_reverse_binary(user_input))
+    binary_string = str(int_to_reverse_binary(num1))
     binary_string = str(string_reverse(binary_string))
     
     print(binary_string)
--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,5 +1,9 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
+   user_input1==user_val2
+   user_input2==user_val1
+   user_input3==user_val4
+   user_input4==user_val3
    
    return user_val1, user_val2, user_val3, user_val4
 
@@ -9,6 +13,7 @@ if __name__ == '__main__':
    user_input3 = int(input())
    user_input4 = int(input())
    #store output for every input here
+   print(swap_values(user_input2, user_input1, user_input4, user_input3))
    #print those output
 
  
